b439de0b149f161a1fdc615db71a2dab45d95697
Chethan <chethan@Chethans-MacBook-Pro.local>
2017-09-07 08:33:05 +0530

    Added app configuration

---
+++ src/api/HeroRouter.ts
@@ -0,0 +1,36 @@
+import {Router,Request,Response,NextFunction} from 'express';
+import Hero = require('../model/Hero');
+
+export class HeroRouter {
+  router: Router
+
+  /**
+   * Initialize the HeroRouter
+   */
+  constructor() {
+    this.router = Router();
+    this.init();
+  }
+
+  /**
+   * GET all Heroes.
+   */
+private getAll(req: Request, res: Response, next: NextFunction) {
+    res.send("");
+  }
+    
+  /**
+   * Take each handler, and attach to one of the Express.Router's
+   * endpoints.
+   */
+  init() {
+    this.router.get('/', this.getAll);
+  }
+
+}
+
+// Create the HeroRouter, and export its configured Express.Router
+const heroRoutes:HeroRouter = new HeroRouter();
+
+
+export default heroRoutes.router;
